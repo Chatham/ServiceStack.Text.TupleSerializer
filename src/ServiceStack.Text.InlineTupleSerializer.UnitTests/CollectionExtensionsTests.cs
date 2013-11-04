cb882d48842a81a3446388ea84a04cea373461d8
Kacper Harabasz <kharabasz@chathamfinancial.eu>
2013-11-04 17:34:44 +0100

    collection extensions tests

---
+++ src/ServiceStack.Text.InlineTupleSerializer.UnitTests/CollectionExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ServiceStack.Text.InlineTupleSerializer.UnitTests
+{
+    [TestClass]
+    public class CollectionExtensionsTests
+    {
+        [TestMethod]
+        public void GetPublicTuples_NullCollection_ReturnsEmptyHashSet()
+        {
+            var hashSet = CollectionExtensions.GetPublicTuples(null);
+            Assert.IsTrue(hashSet.IsEmpty());
+        }
+
+        [TestMethod]
+        public void GetPublicTuples_EmptyCollection_ReturnsEmptyHashSet()
+        {
+            var hashSet = new List<Type>().GetPublicTuples();
+            Assert.IsTrue(hashSet.IsEmpty());
+        }
+    }
+}
