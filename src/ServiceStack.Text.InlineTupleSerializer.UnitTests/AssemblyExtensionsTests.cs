f9e40e7b3b817fe8974e285ca46c1519d71c088e
Kacper Harabasz <kharabasz@chathamfinancial.eu>
2013-11-04 18:39:40 +0100

    some tests around AssemblyExtensions

--- src/ServiceStack.Text.InlineTupleSerializer.UnitTests/AssemblyExtensionsTests.cs
+++ src/ServiceStack.Text.InlineTupleSerializer.UnitTests/AssemblyExtensionsTests.cs
@@ -39,5 +39,33 @@ namespace ServiceStack.Text.InlineTupleSerializer.UnitTests
         {
             Assert.IsTrue(AssemblyExtensions.AlwaysTrueFilter(string.Empty));
         }
+
+        [TestMethod]
+        public void GetPublicTuples_NullCollection_ReturnEmptyHashSet()
+        {
+            var hashSet = AssemblyExtensions.GetPublicTuples(null, null);
+            Assert.IsTrue(hashSet.IsEmpty());
+        }
+
+        [TestMethod]
+        public void GetPublicTuples_EmptyCollection_ReturnEmptyHashSet()
+        {
+            var hashSet = new List<Assembly>().GetPublicTuples(null);
+            Assert.IsTrue(hashSet.IsEmpty());
+        }
+
+        [TestMethod]
+        public void GetPublicTuples_CollectionWithNullAssembly_ShouldSkipInspectingNullAssembly()
+        {
+            var executionCount = 0;
+            Func<string, bool> ExecutionCountFilter = s => {
+                executionCount++;
+                return true;
+            };
+
+            var hashSet = new List<Assembly> { null }.GetPublicTuples(ExecutionCountFilter);
+
+            Assert.AreEqual(0, executionCount);
+        }
     }
 }
