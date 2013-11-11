e7c4e81c7c570c6e157694ccc94fb9f51b3998e7
Kacper Harabasz <kharabasz@chathamfinancial.eu>
2013-11-11 14:09:16 +0100

    Renaming this test case so it is more readable

---
+++ src/ServiceStack.Text.TupleSerializer.UnitTests/TestCases/TupleNestedInGenericArguments.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace NestedInGenerics
+{
+    public class ObjectWithTupleNestedInDefinition_ImplementedGenericInterface : IEnumerable<Tuple<string>>
+    {
+        public IEnumerator<Tuple<string>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class ObjectWithTupleNestedInDefinition_InheritedGenericClass : List<Tuple<int>>
+    {
+    }
+
+    public class ObjectWithTupleNestedInProperty_GenericClass
+    {
+        public List<Tuple<bool, bool>> PairOfBools { get; set; }
+    }
+
+    public class ObjectWithTupleNestedInProperty_GenericInterface
+    {
+        public IList<Tuple<int, int>> PairOfIntegers { get; set; }
+    }
+}
